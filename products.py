09eed78fd3baac884560c765c66059f8237ec3ab
trandinhhiep35-wq <trandinhhiep35@gmail.com>
2026-07-24 21:30:33 +0700

    Create products.py

---
+++ products.py
@@ -0,0 +1,20 @@
+# File: products.py
+
+PRODUCTS_INFO = """
+[THÔNG TIN SHOP & SẢN PHẨM]
+
+1. THÔNG TIN CHUNG:
+- Tên Shop: [Điền tên Shop của ông]
+- Giờ làm việc: 8:00 - 22:00 hàng ngày (Có nhân viên hỗ trợ trực tiếp)
+- Hotline / Zalo: [Điền SĐT Hotline]
+
+2. DANH MỤC SẢN PHẨM & BẢNG GIÁ:
+- Sản phẩm A: 150.000đ (Màu sắc: Đen, Trắng | Size: M, L, XL)
+- Sản phẩm B: 250.000đ (Màu sắc: Xanh, Xám | Size: Freesize)
+- Sản phẩm C: 350.000đ (Màu sắc: Kem, Đen)
+
+3. CHÍNH SÁCH SHIP & ĐỔI TRẢ:
+- Phí vận chuyển: Đồng giá 25.000đ toàn quốc. Miễn phí ship cho đơn từ 500.000đ.
+- Thời gian nhận hàng: Nội thành 1-2 ngày, tỉnh khác 3-5 ngày.
+- Chính sách đổi trả: Hỗ trợ đổi size/mẫu trong vòng 7 ngày kể từ khi nhận hàng (sản phẩm phải còn nguyên tem mác).
+"""
